7eb313ce211b60be29d51da41deeff24e63658e1
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-02-27 16:49:54 +0300

    update to 0.4.82

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
 # Clone Upstream
-git clone https://gitlab.freedesktop.org/pipewire/wireplumber -b 0.4.18
+git clone https://gitlab.freedesktop.org/pipewire/wireplumber -b 0.4.82
 cp -rvf ./debian ./wireplumber/
 cd ./wireplumber
 
 for i in ../patches/*.patch; do patch -Np1 -i $i ;done
+LOGNAME=root dh_make --createorig -y -l -p wireplumber_0.4.82
 
 # Get build deps
 apt-get build-dep ./ -y
